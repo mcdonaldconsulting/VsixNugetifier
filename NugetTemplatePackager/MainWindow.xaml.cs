caeb8a8bba4a32e3a91a3e42a759ad7f51f38779
Patrick McDonald <paddymcdonald@gmail.com>
2012-07-11 23:29:13 +0100

    Initial Commit

---
+++ NugetTemplatePackager/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows;
+using System;
+using System.IO;
+
+namespace NugetTemplatePackager
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            var appDataPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+
+            var tempPath = Path.GetTempPath();
+
+            string path = @"C:\Users\Patrick\Documents\Visual Studio 2010\My Exported Templates\WebApiConsole.vsix.zip";
+            var x = new NugetTemplateTools.ProjectTemplate(path);
+
+            var files = x.Unpack(@"C:\temp");
+
+            foreach (var file in files)
+            {
+                textBox1.Text += file + "\r\n";
+            }
+        }
+    }
+}
---
+++ NugetTemplateTools/ProjectTemplate.cs
@@ -0,0 +1,35 @@
+﻿namespace NugetTemplateTools
+{
+    using System.Collections.Generic;
+    using System.IO.Packaging;
+
+    public class ProjectTemplate
+    {
+        private readonly string filePath;
+
+        public ProjectTemplate(string templateFilePath)
+        {
+            this.filePath = templateFilePath;
+        }
+
+        public IEnumerable<string> Unpack(string targetPath)
+        {
+            using (var package = Package.Open(this.filePath))
+            {
+                var parts = package.GetParts();
+
+                foreach (var part in parts)
+                {
+                    if (part.ContentType == "application/zip")
+                    {
+                        yield return part.Uri.ToString() + " (Zip)";
+                    }
+                    else
+                    {
+                        yield return part.Uri.ToString();
+                    }
+                }
+            }
+        }
+    }
+}
