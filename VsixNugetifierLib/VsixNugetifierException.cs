b6fb1827d8f36a5ddc49547b12c046ddd1bf0f1b
Patrick McDonald <PaddyMcDonald@gmail.com>
2012-07-20 17:06:27 +0100

    Added VsixNugetifierException

---
+++ VsixNugetifierLib/VsixNugetifierException.cs
@@ -0,0 +1,29 @@
+﻿namespace NugetTemplateTools
+{
+    using System;
+    using System.Runtime.Serialization;
+
+    [Serializable]
+    public class VsixNugetifierException : Exception
+    {
+        public VsixNugetifierException()
+            : base()
+        {
+        }
+
+        public VsixNugetifierException(string message)
+            : base(message)
+        {
+        }
+
+        public VsixNugetifierException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+
+        protected VsixNugetifierException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+        }
+    }
+}
